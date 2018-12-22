4470145affece3152f77c8e3103ee4c216040f0b
luko <luko.web@gmail.com>
2018-12-22 00:21:11 -0300

    Add Business components to render method of BusinessList component

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,2 +1,26 @@
-y
-sjhflskdj
+/* BusinessList Component */
+import React from 'react';
+import './BusinessList.css';
+// importa el componente 'Business'
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component {
+  render() {
+    return(
+      // HMTL (JSX)
+      <div className="BusinessList">
+        {Business}
+        {Business}
+        {Business}
+        {Business}
+        {Business}
+        {Business}
+      </div>
+    );
+  }
+}
+
+export default BusinessList;
+
+
+
