04c8af790a3fe244431f649435298017369c6748
luko <luko.web@gmail.com>
2018-12-22 00:36:00 -0300

    Create sortByOptions object in SearchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,1 +1,21 @@
-sdhfhsjk
+/* Search Bar Component */
+import React from 'react';
+import './SearchBar.css';
+
+// un objeto que contiene las diferentes opciones de búsqueda 
+const sortByOptions = {
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
+}
+
+class SearchBar extends React.Component {
+  // render method
+  render() {
+    return(
+
+    )
+  }
+}
+
+export default SearchBar;
