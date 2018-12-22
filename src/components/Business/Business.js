7d73d668386a894d5ebf9724ec275a32161ed374
luko <luko.web@gmail.com>
2018-12-21 23:45:28 -0300

    Fill render method in Business component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -11,3 +11,32 @@ const business = {
   rating: 4.5,
   reviewCount: 90
 }
+
+// las 'instrucciones' para crear un componente 'Business'...
+class Business extends React.Component {
+
+  // render method
+  render() {
+    return (
+      // HTML (JSX)
+      <div className="Business">
+          <div className="image-container">
+            <img src={business.imageSrc} alt=''/>
+          </div>
+          <h2>{business.name}</h2>
+          <div className="Business-information">
+            <div className="Business-address">
+              <p>{business.address}</p>
+              <p>{business.state}</p>
+              <p>{business.zipCode}</p>
+            </div>
+            <div className="Business-reviews">
+              <h3>{business.category}</h3>
+              <h3 className="rating">{business.rating}</h3>
+              <p>{business.reviewCount} reviews</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
