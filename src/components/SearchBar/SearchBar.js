a8ba0c2736b9ef64ffe33804aeb9da8584cdf8cd
luko <luko.web@gmail.com>
2018-12-23 20:11:40 -0300

    Fix renderSortByOptions method in SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -7,20 +7,16 @@ const sortByOptions = {
   'Best Match': 'best_match',
   'Highest Rated': 'rating',
   'Most Reviewed': 'review_count'
-}
+};
 
 class SearchBar extends React.Component {
 
   // render sort by options method
   renderSortByOptions() {
     return Object.keys(sortByOptions).map(sortByOption => {
-      // una variable para almacenar los valores de las claves
-      // de las búsquedas por opción
       let sortByOptionValue = sortByOptions[sortByOption]; // 👏
+      return <li key={sortByOptionValue}>{sortByOption}</li>
     });
-
-    //return <li key={sortByOptionValue}>{sortByOption}</li>
-
   }
   
   // render method
