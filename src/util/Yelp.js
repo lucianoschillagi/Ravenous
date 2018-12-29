cf19ed33db08aba1d93e7fe1dbf7099d0da91d15
luko <luko.web@gmail.com>
2018-12-29 00:26:26 -0300

    Add Yelp object

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,5 +1,6 @@
 /* Yelp API */
 
-const yelp = 'Ssaq3XDGkTE3OqPbfFl8WGpH79L2b4OxDqFz9aUMbzn6n51gCUL4YAlzLyTUBlikI2UduM0MoX9R1SGFF2y48mml5XLeATga1_lBzbVqsM1mtp39yvrjNDkGdLcSXHYx';
+const apiKey = 'Ssaq3XDGkTE3OqPbfFl8WGpH79L2b4OxDqFz9aUMbzn6n51gCUL4YAlzLyTUBlikI2UduM0MoX9R1SGFF2y48mml5XLeATga1_lBzbVqsM1mtp39yvrjNDkGdLcSXHYx';
 
-
+// This object will store the functionality needed to interact with the Yelp API.
+const Yelp = {};
